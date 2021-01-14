ee31958698aa33e05761938aab7afda2fe51ce57
aceh0und <kowacmelvyn@gmail.com>
2021-01-14 00:10:21 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+
+
+async function main() {
+    let driver = await new Builder().forBrowser("firefox").build();
+
+    await driver.get("https://www.swagbucks.com/shop/allcoupons/grocery");
+    
+    /* CANNOT FIND ELEMENT BECAUSE NOT LOGGED IN, BUT CANNOT LOGIN AUTOMATICALLY BECAUSE OF CAPTCHA. I'm stuck :( */
+    currObj = driver.findElement(By.className("coupon"));
+    
+    await currObj.click()
+
+}
+main()
